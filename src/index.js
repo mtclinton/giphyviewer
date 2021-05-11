d60aee32397bca9ee8e0a163bbbbbde7f3fba7da
mtclinton <maxtclinton@gmail.com>
2021-05-11 14:08:59 -0400

    added readt router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Router>
+        <React.StrictMode>
+            <Switch>
+                <Route path="/about">
+                    <App />
+                </Route>
+                <Route path="/users">
+                    <App />
+                </Route>
+                <Route path="/">
+                    <App />
+                </Route>
+            </Switch>
+        </React.StrictMode>,
+        document.getElementById('root')
+    </Router>
+
 );
